8222ae532dce00013fa8d24e040f162ac4886f14
Giovanni Bryden <giovanni.bryden@moovweb.com>
2013-07-27 22:17:30 -0700

    Rebuilt footer. Added dumb text.

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,4 +1,17 @@
 $("./body") {
+
+  # Get image source from piece of shit iframe banner I can't resize
+  remove("//iframe[@name='503f8b354f98f']/ancestor::div[@id='logoBlock']")
+
   insert_bottom("footer", class: "_footer") {
+    move_here("..//aside/div[contains(@class,'sidebar')]/div[contains(@class,'sharePage')]")
+    move_here("../footer/div[contains(@class,'footer')]")
+
+    # Add shameless plug about how I built this
+    insert("div") {
+      text() {
+        set("Built by G. Bryden")
+      }
+    }
   }
 }
